11a1f3a2139a8bcf5fe1148bdc602be6d1d148a1
Arie <a.muhammad9090@gmail.com>
2026-09-11 15:39:26 +0700

    Update minor

--- lib/core/utils/csv_export_web.dart
+++ lib/core/utils/csv_export_web.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+// ignore: avoid_web_libraries_in_flutter, deprecated_member_use
 import 'dart:html' as html;
 
 /// Trigger download file CSV lewat browser. Prefix BOM (`\uFEFF`) biar
